7542f5092e34e8b3f73ea56495005a65e78db6a5
pemarival <mvalenzuelapenagos@gmail.com>
2026-05-12 16:07:04 -0500

    add table terms_conditions and terms_acceptance

--- 05_rollbacks/01_ddl/03_tables/002_create_legal_tables.rollback.sql
+++ 05_rollbacks/01_ddl/03_tables/002_create_legal_tables.rollback.sql
@@ -1,2 +1,4 @@
+DROP TABLE IF EXISTS legal.terms_conditions CASCADE;
+DROP TABLE IF EXISTS legal.terms_acceptance CASCADE;
 DROP TABLE IF EXISTS legal.guardian CASCADE;
 DROP TABLE IF EXISTS legal.consent CASCADE;
