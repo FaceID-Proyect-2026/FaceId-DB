7a5b5c8222c49765bb6bb6d1d5dea518dbffc162
Mariana Valenzuela Penagos <mvalenzuelapenagos@gmail.com>
2026-07-06 14:12:17 -0500

    Update 004_create_environment_tables.sql

--- 01_ddl/03_tables/004_create_environment_tables.sql
+++ 01_ddl/03_tables/004_create_environment_tables.sql
@@ -11,7 +11,23 @@ CREATE TABLE environment.environment (
     deleted_at TIMESTAMPTZ, 
     CONSTRAINT chk_environment_status
         CHECK (status IN ('ACTIVE','INACTIVE'))   
-); 
+);
+
+CREATE TABLE environment.chip_environment (
+    id_chip_environment UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id_chip UUID NOT NULL,
+    id_environment UUID NOT NULL,
+    assignment_date DATE NOT NULL,
+    status VARCHAR(20),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by VARCHAR(100),
+    updated_at TIMESTAMPTZ,
+    updated_by VARCHAR(100),
+    deleted_by VARCHAR(100),
+    deleted_at TIMESTAMPTZ,
+    CONSTRAINT chk_chip_environment_status
+        CHECK (status IN ('ACTIVE','INACTIVE'))
+);
 
 CREATE TABLE environment.record_environment (
     id_record_environment UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
