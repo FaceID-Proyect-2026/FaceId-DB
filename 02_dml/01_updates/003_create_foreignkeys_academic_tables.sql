ddc3c87fbb44b9a2f08b101a6e39059ac1873516
Maria Jose <mariajoserodriguezoyola@gmaail.com>
2026-07-12 15:13:02 -0500

    se agrego la FK de chip a user_chip

--- 02_dml/01_updates/003_create_foreignkeys_academic_tables.sql
+++ 02_dml/01_updates/003_create_foreignkeys_academic_tables.sql
@@ -7,3 +7,8 @@ ALTER TABLE academic.user_chip
 ADD CONSTRAINT fk_user_chip_users
 FOREIGN KEY (id_user_app)
 REFERENCES security.user_app (id_user_app);
+
+ALTER TABLE academic.user_chip
+ADD CONSTRAINT fk_user_chip_chip
+FOREIGN KEY (id_chip)
+REFERENCES academic.chip (id_chip);
