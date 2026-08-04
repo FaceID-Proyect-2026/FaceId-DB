7446dddf2b3c07150d67767e2ab5943850036246
pemarival <mvalenzuelapenagos@gmail.com>
2026-08-04 11:02:01 -0500

    add module 6

--- 01_ddl/04_alter/009_create_foreignkeys_face_recognition.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE security.face_embedding
-    ADD CONSTRAINT fk_face_embedding_user
-    FOREIGN KEY (id_user_app)
-    REFERENCES security.user_app(id_user_app)
-    ON DELETE CASCADE;
---
+++ 05_rollbacks/01_ddl/03_tables/006_create_face_recognition_tables.rollback.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS facialrecognition.biometric_log;
+DROP TABLE IF EXISTS facialrecognition.facial_event;
+DROP TABLE IF EXISTS facialrecognition.user_face;
+DROP TABLE IF EXISTS facialrecognition.device;
