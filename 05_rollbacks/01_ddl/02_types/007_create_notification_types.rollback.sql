98c55e9690a7d1ad551c9e9c1f6f71508372ccc9
pemarival <mvalenzuelapenagos@gmail.com>
2026-08-07 16:17:49 -0500

    complete modules

--- 05_rollbacks/01_ddl/02_types/007_create_notification_types.rollback.sql
+++
@@ -1,5 +0,0 @@
-DROP TYPE IF EXISTS notification.email_send_status;
-DROP TYPE IF EXISTS notification.notification_event_source;
-DROP TYPE IF EXISTS notification.notification_channel;
-DROP TYPE IF EXISTS notification.notification_status;
-DROP TYPE IF EXISTS notification.notification_type;
